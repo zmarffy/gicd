9e0ba9675a2a489ec2689ca3cfb62bf037f947a5
Zeke Marffy <zmarffy@me.com>
2021-04-16 16:19:24 -0400

    class refactor type hints

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # `gicd`
 
-`gicd` is a Python decorator that will automatcially create an issue on GitHub if a decorated function throws an exception. That's it!
+`gicd` contains a Python class that provides a decorator that will automatcially create an issue on GitHub if a decorated function throws an exception. That's it!
 
 ## Requirements
 
@@ -11,9 +11,11 @@
 Decorate a function with it:
 
 ```python
-from gicd import auto_create_issue
+from gicd import GICD
 
-@auto_create_issue(repo_owner="zmarffy", repo_name="cli-test-repo", exceptions=(IndexError, ValueError))
+issue_creator = GICD("zmarffy", "test-repo")
+
+@issue_creator.auto_create_issue(exceptions=[IndexError, ValueError])
 def error_prone_function():
     if input("Don't type anything; just hit enter... "):
         raise ValueError("Explosions!")
@@ -22,35 +24,12 @@ def error_prone_function():
 
 Then call the function:
 
-```text
+```python
 error_prone_function()
 ```
 
 In the off chance that the function raises an exception, it will create a issue in the GitHub repo specified in the decorator.
 
-You can have it do this with all exceptions (default behavior), or, if you specify the `exceptions` parameter, it will only create an issue when those specific exceptions are thrown.
-
-If you want to integrate this info your own app without having to specify the `repo_owner` and `repo_name` keyword arguments in the decorator, you can place a file with a name of your choice in a certain folder, formatted like this example:
-
-```text
-zmarffy
-cli-test-repo
-```
-
-This is a useful file for installers to create, then.
-
-It would make sense for us to call that file `cli-test`, in this example case. You can then use the decorator with only the `app_name` keyword argument, set to the name of the file you created:
-
-```text
-@auto_create_issue(app_name="cli-test", exceptions=(IndexError, ValueError))
-```
-
-The folder you place it in is:
-
-| OS | Folder |
-| --- | --- |
-| Linux | `/etc/ghinfo/` |
-| Windows | `%PROGRAMDATA%\ghinfo\` |
-| macOS | `/Library/Application Support/ghinfo/` |
+You can have the decorator do this with all exceptions (default behavior), or, if you specify the `exceptions` parameter, it will only create an issue when those specific exceptions are thrown.
 
 There's really not that much else to say about this tiny, somewhat humorous, tool.
--- gicd/api.py
+++ gicd/api.py
@@ -1,69 +1,59 @@
 import logging
-import os
-import platform
 import traceback
 from functools import wraps
 from subprocess import check_output
+from typing import Any, List, Optional
 
 import zmtools
 
 LOGGER = logging.getLogger(__name__)
 
 
-def _create_issue(issue_title, issue_body, repo_owner, repo_name, issue_label="crashautoreport"):
-    """Create an issue on GitHub's issues section for a specific repo
+class GICD():
+    
+    def __init__(self, repo_owner: str, repo_name: str) -> None:
+        """Class that provides a function to create an issue on GitHub's issues section for a specific repo
 
-    Args:
-        issue_title (str): The title of the issue to create
-        issue_body (str): The body of the issue to create
-        repo_owner (str): The username of the owner of the repo to create the issue for
-        repo_name (str): The repo name to create the issue for
-        issue_label (str, optional): The label to attach to the issue. Defaults to "crashautoreport".
-
-    Returns:
-        str: The URL of the created issue
-    """
-    return check_output(["gh", "issue", "create", "--title", issue_title, "--body", issue_body, "--label", issue_label, "-R", f"{repo_owner}/{repo_name}"])
+        Args:
+            repo_owner (str): The owner of the repo
+            repo_name (str): The name of the repo
+        """
+        self.repo_owner = repo_owner
+        self.repo_name = repo_name
 
 
-def auto_create_issue(app_name=None, repo_owner=None, repo_name=None, exceptions=None):
-    """Decorator to create a GitHub issue on exception throw
+    def _create_issue(self, issue_title: str, issue_body: str, issue_label: str = "crashautoreport") -> str:
+        """Create an issue on GitHub's issues section for a specific repo
 
-    Args:
-        app_name (str, optional): The app name that relates to the repo. This is used to find the repo name and repo owner if they are not provided. Defaults to None.
-        repo_owner (str): The username of the owner of the repo to create the issue for
-        repo_name (str): The repo name to create the issue for
-        exceptions (list[Exception], optional): The exception types to create an issue for. None means create te issue for any exception. Defaults to None.
-    """
-    def actual_decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except Exception as e:
-                nonlocal app_name, repo_name, repo_owner, exceptions
-                if not exceptions or any(isinstance(e, etype) for etype in exceptions):
-                    if app_name is not None:
-                        if platform.system() == "Darwin":
-                            filename = os.path.join(os.sep, "Library", "Application Support", "ghinfo")
-                        elif platform.system() == "Windows":
-                            filename = os.path.join(os.environ["PROGRAMDATA"], "ghinfo")
-                        else:
-                            filename = os.path.join(
-                                os.sep, "etc", "ghinfo", app_name)
-                        with open(filename) as f:
-                            repo_owner, repo_name = [
-                                l.strip() for l in f.readlines()]
-                    elif repo_owner is None or repo_name is None:
-                        raise ValueError(
-                            "No repo_owner ot repo_name specified")
-                    tb = traceback.format_exc().strip()
-                    issue_title = zmtools.truncate(
-                        tb.split("\n")[-1], length=25)
-                    issue_body = f"```python\n{tb}\n```"
-                    issue = _create_issue(
-                        issue_title, issue_body, repo_owner=repo_owner, repo_name=repo_name)
-                    LOGGER.info(f"An issue was created at {issue}")
-                raise e
-        return wrapper
-    return actual_decorator
+        Args:
+            issue_title (str): The title of the issue to create
+            issue_body (str): The body of the issue to create
+            issue_label (str, optional): The label to attach to the issue. Defaults to "crashautoreport".
+
+        Returns:
+            str: The URL of the created issue
+        """
+        return check_output(["gh", "issue", "create", "--title", issue_title, "--body", issue_body, "--label", issue_label, "-R", f"{self.repo_owner}/{self.repo_name}"])
+
+
+    def auto_create_issue(self, exceptions: Optional[List[Exception]] = None) -> Any:
+        """Decorator to create a GitHub issue on exception throw
+
+        Args:
+            exceptions (list[Exception], optional): The exception types to create an issue for. None means create te issue for any exception. Defaults to None.
+        """
+        def actual_decorator(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    if not exceptions or any(isinstance(e, etype) for etype in exceptions):
+                        tb = traceback.format_exc().strip()
+                        issue_title = zmtools.truncate(
+                            tb.split("\n")[-1], length=25)
+                        issue = self._create_issue(issue_title, f"```python\n{tb}\n```")
+                        LOGGER.info(f"An issue was created at {issue}")
+                    raise e
+            return wrapper
+        return actual_decorator
